45c8961d1a449ac110b0e3103c9c4f19550ca93e
WilmerRojas <wilmer7522@gmail.com>
2024-04-09 12:26:28 -0500

    segundo

--- Dia 1/main.py
+++ Dia 1/main.py
@@ -4,4 +4,47 @@
 
 #IMPRIMIR HOLA MUNDO
 
-Print("Hola mundo")
+print("Hola mundo")
+
+
+#Datos primitivos
+#numero
+numero = 10 #nombrevariable = valorvariable
+print(numero) # imprimir variable
+print(type(numero)) # imprime tipo de variable
+
+#flotante
+numerodecimal = 20.5
+print(numerodecimal)
+print(type(numerodecimal))
+
+#boleano
+boleano = True
+print(boleano)
+print(type(boleano))
+
+#cadena de texto
+texto = "hola Mundo"
+print(texto)
+print(type(texto))
+
+#Ingresa por teclado la infomarcion
+print("ingresa tu nombre")
+nuevo = input() 
+print("Tu nombre es", nuevo)
+
+#Conversion de tipos de variable
+numerico = 10
+print(type(numerico))
+numerico = 15.6
+print(type(numerico))
+numerico = True
+print(type(numerico))
+numerico = "esto es un texto"
+print(type(numerico))
+
+#Bucles For y While
+for 
+#Funciones (4 Tipos)
+
+#Desarrollado por Wilmer Rojas - C.C 134310528
