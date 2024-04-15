64a153bbae5bb84a21b1acf582ca73d2bb4ad65f
Wilmer Rojas <wilmer7522@gmail.com>
2024-04-15 10:09:29 -0500

    corregido

--- Dia 4/money_RojasWilmer.py
+++ Dia 4/money_RojasWilmer.py
@@ -1,17 +1,24 @@
 
-ingreso = int(input("Ingresa el dinero "))
-lista = [10, 5,1]#se crea una lista para definir los valores 
-suma = 0# se crea una variable para sumar la cantidad de monedas que se usaron para dar el cambio
-cant = 0# se crea una variable para hacer el calculo de las monedas necesarias de cada tipo
-for i in lista:
-    if ingreso >=i:#se condiciona lo que ingresa el usuario con el incrementador mientras cumple la condicion
-        cant= ingreso//i#se guarda el valor de la division de lo que ingresa el valor y lo que recorre i
-        suma += cant
-        print("la cantidad de monedas de",i, "son" ,cant)
-        print("")
-        ingreso= ingreso%i#se hace el calculo con el modulo de i en ese ciclo para it descartando los valores creados en lista
+seguir = True
+while seguir:
 
-print("la suma de las monedas es: ",suma)
+    ingreso = int(input("Ingresa el dinero "))
+    lista = [10, 5,1]#se crea una lista para definir los valores 
+    suma = 0# se crea una variable para sumar la cantidad de monedas que se usaron para dar el cambio
+    cant = 0# se crea una variable para hacer el calculo de las monedas necesarias de cada tipo
+    for i in lista:
+        if ingreso >=i:#se condiciona lo que ingresa el usuario con el incrementador mientras cumple la condicion
+            cant= ingreso//i#se guarda el valor de la division de lo que ingresa el valor y lo que recorre i
+            suma += cant
+            print("la cantidad de monedas de",i, "son" ,cant)
+            print("")
+            ingreso= ingreso%i#se hace el calculo con el modulo de i en ese ciclo para it descartando los valores creados en lista
+
+    print("la suma de las monedas es: ",suma)
+    print("")
+    op = str.lower(input("Desea salir Si/No "))
+    if op == "si":
+        seguir = False
 
 #Creado por camper Wilmer Rojas
  
