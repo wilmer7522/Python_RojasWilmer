2c50c75991bbc0be34afc3385b48d9d1691f1d0a
WilmerRojas <wilmer7522@gmail.com>
2024-04-15 20:05:40 -0500

    intentando

---
+++ Dia5/hello.py
@@ -0,0 +1,37 @@
+T = int(input("ingresa la cantidad de casos "))
+lista_n = []
+
+for i in range(T):
+
+    lista = int(input("ingresa el largo de los numeros "))
+    n = str(input(f"ingrese los valores {i + 1} "))
+    for i in range(lista):
+        for j in range(lista):
+            lista_n.append(n)
+        
+
+
+
+
+
+
+
+
+
+n = int(n)
+print("la lista es ",lista_n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
