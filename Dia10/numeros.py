efe3da70f6bf99d56fcf951452996fc74ccd8c56
Wilmer <wilmer7522@gmail.com>
2024-04-22 12:54:40 -0500

    subido

---
+++ Dia10/numeros.py
@@ -0,0 +1,21 @@
+n=input("")
+lista = list(sorted(set(map(int,n.split(",")))))
+lista.sort()
+target =  int(input())
+
+cont=0
+
+lista.append(target)
+lista.sort()
+
+for i in lista:
+    cont+=1
+    if target == i:
+        print(cont-1)
+        break   
+
+
+
+
+
+        
